bd121e66af7492587370307c554e59f84e9ca787
Medruz <luca.medri.stud@ispascalcomandini.it>
2025-10-23 13:58:23 +0200

    Start create Flask app

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-import flask
+from flask import Flask
 
-app = flask.Flask(__name__)
+# Create an instance of Flask 
+app = Flask(__name__)
 
+# Define a route and the function to handle it
+# "/" simboleggia la root
 @app.route('/')
 def home():
     return "Welcome to the Car Sharing Service!"
 
+@app.route('/about')
+def about():
+    return "About this page"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f"User: {username}"
+
+# Run the application if this file is executed directly
 if __name__ == '__main__':
     app.run(debug=True)
