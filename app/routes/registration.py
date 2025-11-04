4729350b92b073652b2980823c0a3915f6b1dcde
Medruz <luca.medri.stud@ispascalcomandini.it>
2025-11-04 22:54:05 +0100

    Create login_page

--- app/routes/registration.py
+++ app/routes/registration.py
@@ -7,8 +7,8 @@ registration_bp = Blueprint("registration", __name__)
 
 @registration_bp.route("/registration_driver")
 def registration_driver():
-    return render_template("index.html")
+    return render_template("login_driver.html")
 
 @registration_bp.route("/registration_passenger")
 def registration_passenger():
-    return render_template("index.html")
+    return render_template("login_passenger.html")
