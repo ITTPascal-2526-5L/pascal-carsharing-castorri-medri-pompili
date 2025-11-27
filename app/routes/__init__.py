429fa8b5e3eb18ae8c81041d33aee0b89e4b2c28
Medruz <luca.medri.stud@ispascalcomandini.it>
2025-11-27 13:36:33 +0100

    Add logout

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,6 +3,7 @@ from .registration import registration_bp
 from .login import login_bp
 from .vehicles import vehicles_bp
 from .user_page import user_bp
+from .logout import logout_bp
 
-blueprints = [main_bp, registration_bp, login_bp, vehicles_bp, user_bp]
+blueprints = [main_bp, registration_bp, login_bp, vehicles_bp, user_bp, logout_bp]
 
