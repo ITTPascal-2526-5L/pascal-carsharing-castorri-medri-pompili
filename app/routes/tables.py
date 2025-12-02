207052dab4853e19a1427c3f82c12b82105d21b8
Medruz <luca.medri.stud@ispascalcomandini.it>
2025-12-02 11:41:48 +0100

    Add tables for passengers and schools

--- app/routes/tables.py
+++ app/routes/tables.py
@@ -11,3 +11,21 @@ def drivers_detail():
         return render_template("tables/drivers_detail.html", drivers=data, message="")
     except: 
         return render_template("tables/drivers_detail.html", message="No drivers saved.")
+    
+@tables_bp.route("/passengers_detail")
+def passengers_detail():
+    try:
+        with open("DataBase/passengers.json", "r", encoding="utf-8")as f:
+            data = json.load(f)
+        return render_template("tables/passengers_detail.html", passengers=data, message="")
+    except: 
+        return render_template("tables/passengers_detail.html", message="No passengers saved.")
+    
+@tables_bp.route("/schools_detail")
+def schools_detail():
+    try:
+        with open("DataBase/schools.json", "r", encoding="utf-8")as f:
+            data = json.load(f)
+        return render_template("tables/schools_detail.html", schools=data, message="")
+    except: 
+        return render_template("tables/schools_detail.html", message="No schools saved.")
